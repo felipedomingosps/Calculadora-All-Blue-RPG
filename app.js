19faf76ced5b3026de8adbb78f78384e620aeb29
Felipe D <felipedomingosps@gmail.com>
2022-12-04 00:28:07 -0300

    Add countPointsUsed() and  displayRemainingAndUsedPoints()

--- app.js
+++ app.js
@@ -1,12 +1,35 @@
-function createFieldset(groupName, groupNameAbb){
-    return console.log(`
-    <fieldset class="form__fieldset">
-        <legend>${groupName}</legend>
-        <div class="form__element"><label for="${groupNameAbb}-forca" class="element__label">Força:</label><input type="number" name="${groupNameAbb}-forca" id="${groupNameAbb}-forca" class="element__input"></div>
-        <div class="form__element"><label for="${groupNameAbb}-destreza" class="element__label">Destreza:</label><input type="number" name="${groupNameAbb}-destreza" id="${groupNameAbb}-destreza" class="element__input"></div>
-        <div class="form__element"><label for="${groupNameAbb}-acerto" class="element__label">Acerto:</label><input type="number" name="${groupNameAbb}-acerto" id="${groupNameAbb}-acerto" class="element__input"></div>
-        <div class="form__element"><label for="${groupNameAbb}-reflexo" class="element__label">Reflexo:</label><input type="number" name="${groupNameAbb}-reflexo" id="${groupNameAbb}-reflexo" class="element__input"></div>
-        <div class="form__element"><label for="${groupNameAbb}-constituicao" class="element__label">Constituição:</label><input type="number" name="${groupNameAbb}-constituicao" id="${groupNameAbb}-constituicao" class="element__input"></div>
-    </fieldset>
-    `)
+
+
+function countPointsUsed() {
+    const attributes = document.querySelectorAll('.form__fieldset--brutos > div > input')
+    const attributesValues = []
+
+    attributes.forEach(attribute => {
+        attributesValues.push(attribute.value)
+    })
+
+    return attributesValues.reduce((accumulator, currentValue) => Number(accumulator) + Number(currentValue))
 }
+
+function displayRemainingAndUsedPoints() {
+    const elements = [...document.querySelectorAll('.form__fieldset--brutos > div > input'), document.querySelector('#principal-experiencia')]
+
+    const displayRemainingPoints = () => {
+        document.getElementById('pontos-usados').innerHTML = countPointsUsed()
+    };
+
+    const displayUsedPoints = () => {
+        document.getElementById('pontos-restantes').innerHTML = document.getElementById('principal-experiencia').value - countPointsUsed()
+    };
+
+    elements.forEach(element => {
+        element.addEventListener('change', () => {
+            displayRemainingPoints()
+            displayUsedPoints()
+        })
+    })
+
+}
+
+displayRemainingAndUsedPoints()
+
